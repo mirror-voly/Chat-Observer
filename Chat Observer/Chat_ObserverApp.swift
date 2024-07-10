bed06bc5331cfa3b42839c2525be7516c9a912ec
nur <77068320+mirror-voly@users.noreply.github.com>
2024-07-10 09:24:06 +0400

    Initial Commit

---
+++ Chat Observer/Chat_ObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_ObserverApp.swift
+//  Chat Observer
+//
+//  Created by mix on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chat_ObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
